5a88267b639ee48a3f989bc871f4e6d0ad9974ce
Artem <footerka2@gmail.com>
2021-06-21 18:27:01 +0300

    Initial commit

---
+++ src/main/java/entity/Users.java
@@ -0,0 +1,34 @@
+package entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ForeignKey;
+import org.hibernate.annotations.Table;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
+@ForeignKey(name = "users")
+@Data//ломбок аннотация: генерирует геттеры, сеттеры, иквалс, хеш код методы
+@NoArgsConstructor//ломбок аннотация: конструктор без аргуметов
+public class Users {
+    @javax.persistence.Id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String login;
+
+    @Column
+    private String email;
+
+
+}
